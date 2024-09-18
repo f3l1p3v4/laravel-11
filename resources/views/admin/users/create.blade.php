694f9520c24f03ccaa13e69b98880109229fbf56
f3l1p3v4 <fvaldez018@gmail.com>
2024-09-18 12:36:00 -0400

    added changed

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,16 +1,16 @@
-@extends('admin.layouts.app')
+@extends(view: 'admin.layouts.app')
 
-@section('title', 'Criar Novo Usuário')
+@section(section: 'title', content: 'Criar Novo Usuário')
 
-@section('content')
-    @include('admin.users.partials.breadcrumb')
+@section(section: 'content')
+    @include(view: 'admin.users.partials.breadcrumb')
     <div class="py-6">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mb-4">
             Novo Usuário
         </h2>
     </div>
     {{-- @include('admin.includes.errors') --}}
-    <form action="{{ route('users.store') }}" method="POST">
-        @include('admin.users.partials.form')
+    <form action="{{ route(name: 'users.store') }}" method="POST">
+        @include(view: 'admin.users.partials.form')
     </form>
 @endsection
